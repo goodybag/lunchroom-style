fef908a3d4d0cdf5bc5efe9d305ecd298628a033
Christoph Dorn <christoph@christophdorn.com>
2015-07-23 13:19:42 -0700

    reboot

--- bin/server.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-var port = process.env.port || 8000;
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/../dist')).listen(port);
-console.log('Listening on port ' + port);
